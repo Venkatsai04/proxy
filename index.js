c0c79bd33e2642cc8cbd177b613f87ddafc1213e
Venkatsai04 <106298746+Venkatsai04@users.noreply.github.com>
2024-05-04 18:42:29 +0530

    added

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+// Configure CORS to allow your frontend's domain
+app.use(cors({ origin: "https://waterx.netlify.app" }));
+
+// Proxy endpoint to fetch data from ESP32 API
+app.get("/api/distance", async (req, res) => {
+    console.log("Received request to /api/distance");
+    try {
+      const response = await axios.get("http://192.168.29.94/distance");
+      console.log("Received response from ESP32:", response.data);
+      res.json(response.data);
+    } catch (error) {
+      console.error("Error fetching distance data:", error.message);
+      res.status(500).send("Error fetching distance data");
+    }
+  });
+  
+app.listen(PORT, () => {
+  console.log(`Proxy server running on port ${PORT}`);
+});
